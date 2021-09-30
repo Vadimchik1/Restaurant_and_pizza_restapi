500908130375d55ee46b808d11a1bf042f1ee836
Vadim Ganitsa <vadimganica@gmail.com>
2021-09-30 14:51:40 +0600

    all ready

--- restaurant_pizza/api/urls.py
+++ restaurant_pizza/api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('restaurants/', views.restaurants_list),
-    path('restaurants/<int:pk>', views.restaurants_detail),
+    path('restaurants/', views.RestaurantList.as_view()),
+    path('restaurants/<int:pk>', views.RestaurantDetail.as_view()),
     path('pizzas/', views.pizzas_list),
     path('pizzas/<int:pk>', views.pizzas_detail),
 
